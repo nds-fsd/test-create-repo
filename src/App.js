5e4b0a6aef7232c051fd58aec33bdf65a9ac05b1
carlosarenasf <carenas@bloobirds.com>
2022-07-08 21:16:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import { MyInput } from 'la-libreria-de-mariela';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MyInput label="hola" type="text" />
     </div>
   );
 }
